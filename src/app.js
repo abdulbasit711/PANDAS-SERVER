3037db53b20beb6f26b04c2bbe16fc660df7222d
abdulbasit711 <145869031+abdulbasit711@users.noreply.github.com>
2025-07-31 20:38:00 +0500

    Update app.js

--- src/app.js
+++ src/app.js
@@ -45,7 +45,7 @@ app.use((err, req, res, next) => {
 
   res.status(statusCode).json({
     success: false,
-    message: err.message || "Internal Server Error",
+    message: err || "Internal Server Error",
     errors: err.errors || [],
     stack: process.env.NODE_ENV === "production" ? undefined : err.stack,
   });
